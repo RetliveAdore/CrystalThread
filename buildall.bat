f0c73c74f3f8e58cd98c16f6add67c8e344b0521
RetliveAdore <lizaterop@gmail.com>
2024-07-10 22:51:52 +0800

    修改编译脚本

--- buildall.bat
+++ buildall.bat
@@ -1,4 +1,5 @@
 @if not exist CrystalCore\ (git submodule add https://github.com/RetliveAdore/CrystalCore)
+@git submodule update --init
 @git submodule update --remote
 @copy .\platform\windows\makefile .\makefile
 @mingw32-make build
--- buildall.sh
+++ buildall.sh
@@ -1,5 +1,6 @@
 if [ ! -d "./CrystalCore/" ];then git submodule add https://github.com/RetliveAdore/CrystalCore;fi
 if [ ! -d "./CrystalCore/" ];then git submodule add https://github.com/RetliveAdore/CrystalAlgorithms;fi
+git submodule update --init
 git submodule update --remote
 cp ./platform/linux/makefile ./makefile
 make build
